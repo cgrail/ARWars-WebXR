ad4315eaff65ac9e3fc0fed9b0b63a1578242e70
Christian Grail <christian.grail@sap.com>
2021-06-29 17:51:00 +0200

    ARHelper

--- web/lib/ARHelper.js
+++ web/lib/ARHelper.js
@@ -1,67 +1,51 @@
-import { ARButton } from './threejs/ARButton.js';
+import * as THREE from "./threejs/three.module.js";
+import { ARButton } from "./threejs/ARButton.js";
 
-let container;
-let controller;
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(
+  70,
+  window.innerWidth / window.innerHeight,
+  0.01,
+  20
+);
+const renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
 
-function init(THREE, scene, camera, renderer, render, reticle) {
+function initAR(render, onSelect) {
+  const container = document.createElement("div");
+  document.body.appendChild(container);
 
-    container = document.createElement( 'div' );
-    document.body.appendChild( container );
+  const light = new THREE.HemisphereLight(0xffffff, 0xbbbbff, 1);
+  light.position.set(0.5, 1, 0.25);
+  scene.add(light);
 
+  renderer.setPixelRatio(window.devicePixelRatio);
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.xr.enabled = true;
+  container.appendChild(renderer.domElement);
 
-    const light = new THREE.HemisphereLight( 0xffffff, 0xbbbbff, 1 );
-    light.position.set( 0.5, 1, 0.25 );
-    scene.add( light );
+  document.body.appendChild(
+    ARButton.createButton(renderer, { requiredFeatures: ["hit-test"] })
+  );
 
-    renderer.setPixelRatio( window.devicePixelRatio );
-    renderer.setSize( window.innerWidth, window.innerHeight );
-    renderer.xr.enabled = true;
-    container.appendChild( renderer.domElement );
+  window.addEventListener("resize", function () {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
 
-    document.body.appendChild( ARButton.createButton( renderer, { requiredFeatures: [ 'hit-test' ] } ) );
+    renderer.setSize(window.innerWidth, window.innerHeight);
+  });
 
-    const geometry = new THREE.CylinderGeometry( 0.1, 0.1, 0.2, 32 ).translate( 0, 0.1, 0 );
+  const controller = renderer.xr.getController(0);
+  controller.addEventListener("select", onSelect);
+  scene.add(controller);
 
-    function onSelect() {
+  renderer.setAnimationLoop(render);
 
-        if ( reticle.visible ) {
-
-            const material = new THREE.MeshPhongMaterial( { color: 0xffffff * Math.random() } );
-            const mesh = new THREE.Mesh( geometry, material );
-            mesh.position.setFromMatrixPosition( reticle.matrix );
-            mesh.scale.y = Math.random() * 2 + 1;
-            scene.add( mesh );
-
-        }
-
-    }
-
-    controller = renderer.xr.getController( 0 );
-    controller.addEventListener( 'select', onSelect );
-    scene.add( controller );
-
-    reticle = new THREE.Mesh(
-        new THREE.RingGeometry( 0.15, 0.2, 32 ).rotateX( - Math.PI / 2 ),
-        new THREE.MeshBasicMaterial()
-    );
-    reticle.matrixAutoUpdate = false;
-    reticle.visible = false;
-    scene.add( reticle );
-
-    window.addEventListener( 'resize', function (){
-
-        camera.aspect = window.innerWidth / window.innerHeight;
-        camera.updateProjectionMatrix();
-    
-        renderer.setSize( window.innerWidth, window.innerHeight );
-    } );
-
-    renderer.setAnimationLoop( render );
-
+  return {
+    THREE,
+    scene,
+    camera,
+    renderer,
+  };
 }
 
-
-
-export {
-    init
-}
+export { initAR };
